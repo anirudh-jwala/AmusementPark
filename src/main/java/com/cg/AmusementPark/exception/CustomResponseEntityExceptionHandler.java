7da598fb5e30578bc9780c3d261f28c9680ef108
Anirudh Jwala <anirudhsai2@gmail.com>
2021-04-14 23:38:16 +0530

    Code refactoring
    Moved resource not found to exception package, basic junit test case running

--- src/main/java/com/cg/AmusementPark/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/cg/AmusementPark/exception/CustomResponseEntityExceptionHandler.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.cg.AmusementPark.service.ResourceNotFoundException;
-
 @RestControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -79,16 +77,16 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.CONFLICT);
 
 	}
-	
+
 	@ExceptionHandler({ TicketBookingNotFoundException.class })
 	public final ResponseEntity<Object> handleTicketBookingNotFoundException(TicketBookingNotFoundException exception,
 			WebRequest req) {
 
 		ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(),
-				"The Ticket Booking Request which you are trying to perform operation is not present in the database", new Date());
+				"The Ticket Booking Request which you are trying to perform operation is not present in the database",
+				new Date());
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
 
 	}
-
 
 }
--- src/main/java/com/cg/AmusementPark/exception/ResourceNotFoundException.java
+++ src/main/java/com/cg/AmusementPark/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.cg.AmusementPark.service;
+package com.cg.AmusementPark.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
