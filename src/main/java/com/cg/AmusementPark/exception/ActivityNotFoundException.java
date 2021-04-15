3480d4660ac24a402a755f38287e36befa03a8e9
Anirudh Jwala <anirudhsai2@gmail.com>
2021-04-15 12:16:53 +0530

    Logging added to exception classes

--- src/main/java/com/cg/AmusementPark/exception/ActivityNotFoundException.java
+++ src/main/java/com/cg/AmusementPark/exception/ActivityNotFoundException.java
@@ -1,11 +1,16 @@
 package com.cg.AmusementPark.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ActivityNotFoundException extends Exception {
 
+	private static final Logger logger = LoggerFactory.getLogger(ResourceNotFoundException.class);
 	private static final long serialVersionUID = 1L;
 
 	public ActivityNotFoundException(String message) {
 		super(message);
+		logger.info(message);
 	}
 
 }
--- src/main/java/com/cg/AmusementPark/exception/CustomerExistsException.java
+++ src/main/java/com/cg/AmusementPark/exception/CustomerExistsException.java
@@ -1,11 +1,16 @@
 package com.cg.AmusementPark.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CustomerExistsException extends Exception {
 
+	private static final Logger logger = LoggerFactory.getLogger(ResourceNotFoundException.class);
 	private static final long serialVersionUID = 1L;
 
 	public CustomerExistsException(String message) {
 		super(message);
+		logger.info(message);
 	}
 
 }
