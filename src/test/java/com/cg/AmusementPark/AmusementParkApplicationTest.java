27c5ad43b35186e9467411546f7fd9a4f929b777
Anirudh Jwala <anirudhsai2@gmail.com>
2021-04-18 12:54:12 +0530

    Test cases

--- src/test/java/com/cg/AmusementPark/AmusementParkApplicationTest.java
+++ src/test/java/com/cg/AmusementPark/AmusementParkApplicationTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDate;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AmusementParkApplication.class)
 @WebAppConfiguration
@@ -40,112 +43,89 @@ class AmusementParkApplicationTest {
 	/**
 	 * Customer testing
 	 */
-//	@Test
-//	public void shouldCheckCustomerEmailById() throws Exception {
-//		this.mockMvc
-//				.perform(get("/customer/{id}", 1))
-//				.andDo(print())
-//				.andExpect((ResultMatcher) jsonPath("email", is("sanjay@gmail.com")));
-//	}
-//
-//	@Test
-//	public void shouldCheckCustomerDetails() throws Exception {
-//		this.mockMvc
-//				.perform(get("/customer"))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.[0].customerId").exists())
-//				.andExpect(jsonPath("$.[0].mobileNumber", is("9848012345")));
-//	}
-//
-//	@Test
-//	public void shouldCheckOneCustomerNameAndTicketDetails() throws Exception {
-//		this.mockMvc
-//				.perform(get("/customer/{id}", 1))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.username", is("sanjay")))
-//				.andExpect(jsonPath("$.tickets[0].ticketId", is(1)));
-//	}
-//
+	@Test
+	@Order(value = 1)
+	public void shouldCheckCustomerEmailById() throws Exception {
+		this.mockMvc.perform(get("/customer/{id}", 1)).andDo(print())
+				.andExpect((ResultMatcher) jsonPath("email", is("sanjay@gmail.com")));
+	}
+
+	@Test
+	@Order(value = 2)
+	public void shouldCheckCustomerDetails() throws Exception {
+		this.mockMvc.perform(get("/customer")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.[0].customerId").exists())
+				.andExpect(jsonPath("$.[0].mobileNumber", is("9848012345")));
+	}
+
+	@Test
+	@Order(value = 3)
+	public void shouldCheckOneCustomerNameAndTicketDetails() throws Exception {
+		this.mockMvc.perform(get("/customer/{id}", 1)).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.username", is("sanjay"))).andExpect(jsonPath("$.tickets[0].ticketId", is(2)));
+	}
+
 //	@Test
 //	public void shouldAddCustomer() throws Exception {
 //		this.mockMvc
 //				.perform(post("/customer")
-//				.content(asJsonString(new Customer(6, "saitejac", "sai@gmail.com", "saiteja123", "Hyderabad", "9848012345")))
-//			    .contentType(MediaType.APPLICATION_JSON)
-//			    .accept(MediaType.APPLICATION_JSON))
-//			    .andExpect(status().isCreated())
-//			    .andExpect(jsonPath("$.customerId", is(6)));
+//						.content(asJsonString(
+//								new Customer(6, "saitejac", "sai@gmail.com", "saiteja123", "Hyderabad", "9848012345")))
+//						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isCreated()).andExpect(jsonPath("$.customerId", is(6)));
 //	}
 //
 //	@Test
 //	public void shouldUpdateCustomer() throws Exception {
 //		this.mockMvc
 //				.perform(put("/customer")
-//				.content(asJsonString(new Customer(6, "sai teja", "sai@capgemini.com", "sai123", "Hyderabad", "9848012345")))
-//			    .contentType(MediaType.APPLICATION_JSON)
-//			    .accept(MediaType.APPLICATION_JSON))
-//			    .andExpect(status().isOk())
-//			    .andExpect(jsonPath("$.email", is("sai@capgemini.com")));
+//						.content(asJsonString(
+//								new Customer(6, "sai teja", "sai@capgemini.com", "sai123", "Hyderabad", "9848012345")))
+//						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk()).andExpect(jsonPath("$.email", is("sai@capgemini.com")));
 //	}
-//
+
 //	@Test
 //	public void shouldDeleteCustomer() throws Exception {
-//		this.mockMvc
-//				.perform(delete("/customer/{id}", 5))
-//				.andExpect(status().isOk());
+//		this.mockMvc.perform(delete("/customer/{id}", 5)).andExpect(status().isOk());
 //	}
-//	
+
 	/**
 	 * Activity testing
 	 */
-//	@Test
-//	public void shouldGetActivitiesOfCharges() throws Exception {
-//		this.mockMvc
-//				.perform(get("/activity/{amount}", 500.0f))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.[0].activityId").exists())
-//				.andExpect(jsonPath("$.[0].activityName", is("Jungle Safari")));
-//	}
-//	
-//	@Test
-//	public void shouldGetCountOfActivitiesOfCharges() throws Exception {
-//		this.mockMvc
-//				.perform(get("/activity/{amount}", 500.0f))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(content().string(containsString("2")));
-//	}
-//	
+	@Test
+	public void shouldGetActivitiesOfCharges() throws Exception {
+		this.mockMvc.perform(get("/activity/{amount}", 500.0f)).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.[0].activityId").exists())
+				.andExpect(jsonPath("$.[0].activityName", is("Jungle Safari")));
+	}
+
+	@Test
+	public void shouldGetCountOfActivitiesOfCharges() throws Exception {
+		this.mockMvc.perform(get("/activity/{amount}", 500.0f)).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("2")));
+	}
+
 //	@Test
 //	public void shouldAddActivity() throws Exception {
 //		this.mockMvc
-//				.perform(post("/activity")
-//				.content(asJsonString(new Activity(6, "Gaint wheel", "For adults", 550.0f)))
-//			    .contentType(MediaType.APPLICATION_JSON)
-//			    .accept(MediaType.APPLICATION_JSON))
-//			    .andExpect(status().isCreated())
-//			    .andExpect(jsonPath("$.activityId", is(6)));
+//				.perform(post("/activity").content(asJsonString(new Activity(6, "Gaint wheel", "For adults", 550.0f)))
+//						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isCreated()).andExpect(jsonPath("$.activityId", is(6)));
 //	}
-//	
+//
 //	@Test
 //	public void shouldUpdateActivity() throws Exception {
 //		this.mockMvc
 //				.perform(put("/activity")
-//				.content(asJsonString(new Activity(5, "Lazer show", "For kids and adults", 450.0f)))
-//			    .contentType(MediaType.APPLICATION_JSON)
-//			    .accept(MediaType.APPLICATION_JSON))
-//			    .andExpect(status().isOk())
-//			    .andExpect(jsonPath("$.charges", is(450.0)));
+//						.content(asJsonString(new Activity(5, "Lazer show", "For kids and adults", 450.0f)))
+//						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk()).andExpect(jsonPath("$.charges", is(450.0)));
 //	}
-//	
+
 //	@Test
 //	public void shouldDeleteActivity() throws Exception {
-//		this.mockMvc
-//				.perform(delete("/activity/{id}", 4))
-//				.andExpect(status().isOk());
+//		this.mockMvc.perform(delete("/activity/{id}", 4)).andExpect(status().isOk());
 //	}
 
 	/**
@@ -166,25 +146,11 @@ class AmusementParkApplicationTest {
 				.andExpect(status().isOk()).andExpect(content().string(containsString("500")));
 	}
 
-	@Test
-	public void shouldDeleteTicketBooking() throws Exception {
-
-		this.mockMvc
-				.perform(delete("/ticket/{id}", 1))
-				.andExpect(status().isOk());
-
-	}
-	
 //	@Test
-//	public void shouldAddTicketBooking() {
-//		
-//		this.mockMvc
-//				.perform(post("/ticket"))
-//				.content(asJsonString(new TicketBooking(3, new LocalDate(), 500.0f)))
-//			    .contentType(MediaType.APPLICATION_JSON)
-//			    .accept(MediaType.APPLICATION_JSON))
-//			    .andExpect(status().isCreated());
-//		
+//	public void shouldDeleteTicketBooking() throws Exception {
+//
+//		this.mockMvc.perform(delete("/ticket/{id}", 1)).andExpect(status().isOk());
+//
 //	}
 
 	public String asJsonString(Object obj) {
--- src/test/java/com/cg/AmusementPark/service/TicketBookingServiceTest.java
+++ src/test/java/com/cg/AmusementPark/service/TicketBookingServiceTest.java
@@ -1,11 +1,19 @@
 package com.cg.AmusementPark.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.cg.AmusementPark.entities.Activity;
+import com.cg.AmusementPark.entities.Customer;
 import com.cg.AmusementPark.entities.TicketBooking;
 import com.cg.AmusementPark.repository.TicketBookingRepository;
 
@@ -14,13 +22,33 @@ class TicketBookingServiceTest {
 
 	@Mock
 	private TicketBookingRepository ticketBookingRepository;
-	
+
 	@InjectMocks
 	private TicketBookingService ticketBookingService;
-	
+
 	@Test
-	void shouldAddTicketBooking() {
-		
+	void shouldAddTicketBookings() {
+		LocalDate date = null;
+
+		Customer mockCustomer = new Customer(1, "anirudh", "anirudh@gmail.com", "anirudh123", "Hyderabad",
+				"7981970397");
+
+		List<Activity> mockActivities = new ArrayList<>();
+		mockActivities.add(new Activity(1, "Swimming", "Best for summer", 500.0f));
+		mockActivities.add(new Activity(2, "Gaint Wheel", "Enjoy with family", 350.0f));
+
+		TicketBooking mockTicket = new TicketBooking(1, date, 850.0f, mockCustomer, mockActivities);
+
+		ticketBookingRepository.save(mockTicket);
+
+		Mockito.when(ticketBookingRepository.save(mockTicket)).thenReturn(mockTicket);
+
+		TicketBooking realTicket = ticketBookingService
+				.insertTicketBooking(new TicketBooking(1, date, 850.0f, mockCustomer, mockActivities));
+
+		Assert.assertEquals("Swimming", realTicket.getActivities().get(0).getActivityName());
+		Assert.assertEquals("Enjoy with family", realTicket.getActivities().get(1).getDescription());
+
 	}
 
 }
